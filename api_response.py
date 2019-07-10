ea0ba5a33da4806472b193982e3fe352a706725d
Robert Hughes <robert.hughes4@ocado.com>
2019-07-10 08:04:14 +0100

    Decorator for api gateway responses in a lambda

---
+++ api_response.py
@@ -0,0 +1,40 @@
+from functools import wraps
+import json
+from pprint import pprint
+
+
+def http_response(func):
+    '''Return an AWS Lambda API Gateway response'''
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return {'headers': headers(),
+                'statusCode': 200,
+                'body': json.dumps(func(*args, **kwargs))}
+
+    return wrapper
+
+
+def headers(origin='', cert_valid_for=31536000):
+    """
+    :param origin: Originating domains that are allowed to display this content
+    :param cert_valid_for: Time in seconds the certificate is considered valid
+    :return: Sensible header defaults
+    """
+    return {
+        'Access-Control-Allow-Origin': origin,
+        'Content-Security-Policy': 'default-src null null',
+        'Expect-CT': f'enforce,max-age={cert_valid_for}',
+        'Referrer-Policy': 'no-referrer',
+        'Strict-Transport-Security': f'max-age={cert_valid_for}',
+        'X-Content-Type-Options': 'nosniff',
+        'X-Frame-Options': 'DENY',
+        'X-XSS-Protection': '1; mode=block'}
+
+
+@http_response
+def get_stock():
+    return {'apples': 3, 'pears': 4}
+
+
+if __name__ == '__main__':
+    pprint(get_stock())
